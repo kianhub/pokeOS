29fc70b97b5b1b0d17272b7a2716a8e769698cc2
Kian <kian.pasani@gmail.com>
2026-03-05 14:28:43 -0600

    Wire AnimationEngine into OverlayWindow with delegate-driven movement and state changes

--- pokeOS/Views/OverlayWindow.swift
+++ pokeOS/Views/OverlayWindow.swift
@@ -1,7 +1,8 @@
 import AppKit
 
-class OverlayWindow: NSWindow {
+class OverlayWindow: NSWindow, AnimationEngineDelegate {
     private let overlayContentView: OverlayContentView
+    private let animationEngine = AnimationEngine()
 
     init(contentRect: NSRect) {
         overlayContentView = OverlayContentView(frame: contentRect)
@@ -20,7 +21,10 @@ class OverlayWindow: NSWindow {
         isReleasedWhenClosed = false
         contentView = overlayContentView
 
+        animationEngine.delegate = self
         loadInitialSprite()
+        updateEngineBounds()
+        animationEngine.start()
     }
 
     private func loadInitialSprite() {
@@ -30,6 +34,36 @@ class OverlayWindow: NSWindow {
             let centerX = (frame.width - spriteSize.width) / 2
             let centerY = (frame.height - spriteSize.height) / 2
             overlayContentView.updateSpritePosition(NSPoint(x: centerX, y: centerY))
+            animationEngine.position = CGPoint(x: centerX, y: centerY)
+        }
+    }
+
+    private func updateEngineBounds() {
+        let spriteSize = overlayContentView.spriteView.frame.size
+        let contentRect = self.contentRect(forFrameRect: frame)
+        let insetRect = CGRect(
+            x: 0,
+            y: 0,
+            width: contentRect.width - spriteSize.width,
+            height: contentRect.height - spriteSize.height
+        )
+        animationEngine.updateBounds(insetRect)
+    }
+
+    // MARK: - AnimationEngineDelegate
+
+    func animationEngineDidUpdatePosition(_ position: CGPoint, facingLeft: Bool) {
+        overlayContentView.updateSpritePosition(NSPoint(x: position.x, y: position.y))
+        if facingLeft {
+            overlayContentView.spriteView.layer?.setAffineTransform(CGAffineTransform(scaleX: -1, y: 1))
+        } else {
+            overlayContentView.spriteView.layer?.setAffineTransform(.identity)
+        }
+    }
+
+    func animationEngineDidChangeState(isWalking: Bool) {
+        if let image = SpriteLoader.loadSprite(name: "pikachu", gen: 1, isShiny: false, isWalking: isWalking) {
+            overlayContentView.updateSpriteImage(image)
         }
     }
 }
